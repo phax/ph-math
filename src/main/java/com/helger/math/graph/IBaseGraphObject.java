3606ba356e91f935898f1e09f189d2ab992daa78
phax <ph@phloc.com>
2014-08-24 12:38:21 +0200

    Initial version

---
+++ src/main/java/com/helger/math/graph/IBaseGraphObject.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems (www.phloc.com)
+ * Copyright (C) 2014 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.math.graph;
+
+import com.helger.commons.collections.attrs.IAttributeContainer;
+import com.helger.commons.id.IHasID;
+
+/**
+ * Base interface for graph nodes and graph relations.
+ * 
+ * @author Philip Helger
+ */
+public interface IBaseGraphObject extends IHasID <String>, IAttributeContainer
+{
+  /**
+   * Check if the object is directed or undirected. Directed nodes must
+   * implement {@link IDirectedGraphNode} whereas undirected relations must
+   * implement {@link IGraphNode}. Directed relations must implement
+   * {@link IDirectedGraphRelation} whereas undirected relations must implement
+   * {@link IGraphRelation}.
+   * 
+   * @return <code>true</code> if it is a directed object "from" and "to"),
+   *         <code>false</code> if it is an undirected object.
+   */
+  boolean isDirected ();
+}
---
+++ src/main/java/com/helger/math/graph/simple/SimpleGraphObjectFastFactory.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems (www.phloc.com)
+ * Copyright (C) 2014 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.math.graph.simple;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.helger.math.graph.IGraphNode;
+import com.helger.math.graph.IGraphObjectFactory;
+import com.helger.math.graph.IGraphRelation;
+import com.helger.math.graph.impl.GraphNodeFast;
+import com.helger.math.graph.impl.GraphRelationFast;
+
+/**
+ * Default implementation of the {@link IGraphObjectFactory} with
+ * {@link GraphNodeFast} and {@link GraphRelationFast}.
+ * 
+ * @author Philip Helger
+ */
+public class SimpleGraphObjectFastFactory implements IGraphObjectFactory
+{
+  @Nonnull
+  public IGraphNode createNode ()
+  {
+    return new GraphNodeFast ();
+  }
+
+  @Nonnull
+  public IGraphNode createNode (@Nullable final String sID)
+  {
+    return new GraphNodeFast (sID);
+  }
+
+  @Nonnull
+  public IGraphRelation createRelation (@Nonnull final IGraphNode aFrom, @Nonnull final IGraphNode aTo)
+  {
+    return new GraphRelationFast (aFrom, aTo);
+  }
+
+  @Nonnull
+  public IGraphRelation createRelation (@Nullable final String sID,
+                                        @Nonnull final IGraphNode aFrom,
+                                        @Nonnull final IGraphNode aTo)
+  {
+    return new GraphRelationFast (sID, aFrom, aTo);
+  }
+}
